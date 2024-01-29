5177eba977191c0104ea7427b5629dfa67071970
Lexie <lexie.ejezie@gmail.com>
2024-01-30 00:51:20 +0100

    Complete Card Section

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+// IMAGE SLIDER
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg'];
+var i = 0; // Current Image Index
+
+
+function prev(){
+    if(i <= 0) i = images.length;
+    i--;
+    return setImg();
+}
+
+
+function next(){
+    if(i >= images.length-1) i = -1;
+    i++;
+    return setImg();
+}
+
+function setImg(){
+    return slider_img.setAttribute('src', 'Assets/' + images[i]);
+}
